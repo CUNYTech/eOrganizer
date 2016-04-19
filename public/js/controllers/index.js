f5258c8b3a7dcdbd66a69dddbcd6db03d03798aa
susanjiang03 <susanjiang03@gmail.com>
2016-04-19 19:50:21 -0400

    add different event logos

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -2,39 +2,5 @@
  angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
 
-    // $scope.updateProfile = function(){
-
-
-    //     if (!user.updated) {
-    //         user.updated = [];
-    //     }
-
-    //     user.updated.push(new Date().getTime());
-
-    //     user.$update(function() {
-    //         $location.path('profile/profile');
-    //     });
-
-    // };
-
-
-//Updating Laptop to Computer
-// $scope.updateProfile = function(){
-//     Users.find({where:{id:global.user.id}}).complete(function (err, data) {
-//   if(err){
-//     console.log(err);
-//   }
-//   if(data){
-//     data.updateAttributes({
-//     name: global.user.name,
-//     email: global.user.name,
-//     school : global.user.school,
-//     major : global.user.major
-
-//   }).success(function (data1) {
-//     console.log(data1);
-// //   })
-//  };
-
 
 }]);
